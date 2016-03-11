6c1b6c401d38aee94815dc09347fee34f60172df
Marek Štípek <mario.dweller@seznam.cz>
2016-03-11 01:24:51 +0100

    Added test for autowiring of cached type parser

--- tests/Fixtures/AutowiredServices.php
+++ tests/Fixtures/AutowiredServices.php
@@ -1,18 +1,23 @@
 <?php
 namespace Vanio\VanioDiExtraBundle\Tests\Fixtures;
 
+use Vanio\TypeParser\Parser;
 use Vanio\VanioDiExtraBundle\DependencyInjection\Metadata\MetadataFactory;
 
 class AutowiredServices
 {
-    /** @var MetadataFactory */
+    /** @var Parser|null */
+    public $typeParser;
+
+    /** @var MetadataFactory|null */
     public $metadataFactory;
 
-    /** @var Foo */
+    /** @var Foo|null */
     public $foo;
 
-    public function __construct(MetadataFactory $metadataFactory, Foo $foo)
+    public function __construct(Parser $typeParser = null, MetadataFactory $metadataFactory = null, Foo $foo = null)
     {
+        $this->typeParser = $typeParser;
         $this->metadataFactory = $metadataFactory;
         $this->foo = $foo;
     }
--- tests/Functional/AutowiringTest.php
+++ tests/Functional/AutowiringTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Vanio\VanioDiExtraBundle\Tests\DependencyInjection\Metadata;
 
+use Vanio\TypeParser\CachingParser;
 use Vanio\VanioDiExtraBundle\DependencyInjection\Metadata\CachingMetadataFactory;
 use Vanio\VanioDiExtraBundle\Tests\Fixtures\AutowiredServices;
 use Vanio\VanioDiExtraBundle\Tests\Fixtures\Foo;
@@ -10,6 +11,7 @@ class AutowiringTest extends KernelTestCase
 {
     function test_autowiring()
     {
+        $this->assertInstanceOf(CachingParser::class, $this->autowiredServices()->typeParser);
         $this->assertInstanceOf(CachingMetadataFactory::class, $this->autowiredServices()->metadataFactory);
         $this->assertInstanceOf(Foo::class, $this->autowiredServices()->foo);
     }
